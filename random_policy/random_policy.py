14c4bd137e2fe141fb228a5af0d4fd852b384ef2
Victor I. Afolabi <javafolabi@gmail.com>
2018-09-08 21:39:11 +0100

    Random Policy for a BipedalWalker-v2 agent.

--- random_policy/random_policy.py
+++ random_policy/random_policy.py
@@ -20,20 +20,41 @@ import gym.spaces
 
 class RandomPolicy(object):
 
-  def __init__(self):
-      pass
+    def __init__(self):
+        pass
 
-  def __repr__(self):
-      return 'RandomPolicy()'
-  
-  def __str__(self):
-      return self.__repr__()
+    def __repr__(self):
+        return 'RandomPolicy()'
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __call__(self, env):
+        action = env.action_space.sample()
+        return action
 
 
 def main():
-    # noinspection PyUnresolvedReferences
-    env = gym.make('FrozenLake-v0')
-    env.render()
+    # Game environment.
+    env = gym.make('BipedalWalker-v2')
+
+    episodes, frames = 20, 100
+    policy = RandomPolicy()
+
+    for episode in range(episodes):
+        state = env.reset()
+        done, total_reward = False, 0
+
+        for f in range(frames):
+            env.render()
+
+            # Take random actions.
+            action = policy(env)
+            state, reward, done, info = env.step(action)
+
+            total_reward += reward
+
+        print('Episode {}\tTotal reward: {}'.format(episode + 1, total_reward))
 
 
 if __name__ == '__main__':
